f8a458d14ff870eb6c2a2bf98cb1128eb3010659
benranderson <ben.m.randerson@gmail.com>
2019-10-10 23:43:41 +0100

    add update user route handler

--- demo/api/users/views.py
+++ demo/api/users/views.py
@@ -59,6 +59,30 @@ class Users(Resource):
         }
         return response_object, 200
 
+    def put(self, user_id):
+        post_data = request.get_json()
+        response_object = {"status": "fail", "message": "Invalid payload."}
+        if not post_data:
+            return response_object, 400
+        username = post_data.get("username")
+        email = post_data.get("email")
+        if not username or not email:
+            return response_object, 400
+        try:
+            user = User.query.filter_by(id=int(user_id)).first()
+            if not user:
+                response_object["message"] = "User does not exist."
+                return response_object, 404
+            user.username = username
+            user.email = email
+            db.session.commit()
+            response_object["status"] = "success"
+            response_object["message"] = f"{user.id} was updated!"
+            return response_object, 200
+        except exc.IntegrityError:
+            db.session.rollback()
+            return response_object, 400
+
     def delete(self, user_id):
         response_object = {"status": "fail", "message": "User does not exist"}
         try:
--- tests/test_users.py
+++ tests/test_users.py
@@ -50,30 +50,6 @@ class TestUsersList:
         assert "Sorry. That email already exists." in data["message"]
         assert "fail" in data["status"]
 
-    def test_remove_user(self, client, test_db):
-        recreate_db()
-        user = add_user("user-to-be-removed", "remove-me@email.io")
-        resp_one = client.get("/users")
-        data = json.loads(resp_one.data.decode())
-        assert resp_one.status_code == 200
-        assert len(data["data"]["users"]) == 1
-        resp_two = client.delete(f"/users/{user.id}")
-        data = json.loads(resp_two.data.decode())
-        assert resp_two.status_code == 200
-        assert "remove-me@email.io was removed!" in data["message"]
-        assert "success" in data["status"]
-        resp_three = client.get("/users")
-        data = json.loads(resp_three.data.decode())
-        assert resp_three.status_code == 200
-        assert len(data["data"]["users"]) == 0
-
-    def test_remove_user_incorrect_id(self, client, test_db):
-        resp = client.delete("/users/999")
-        data = json.loads(resp.data.decode())
-        assert resp.status_code == 404
-        assert "User does not exist" in data["message"]
-        assert "fail" in data["status"]
-
 
 class TestUsers:
     def test_single_user(self, client, test_db):
@@ -112,3 +88,76 @@ class TestUsers:
         assert "fletcher" in data["data"]["users"][1]["username"]
         assert "fletcher@notreal.com" in data["data"]["users"][1]["email"]
         assert "success" in data["status"]
+
+    def test_remove_user(self, client, test_db):
+        recreate_db()
+        user = add_user("user-to-be-removed", "remove-me@email.io")
+        resp_one = client.get("/users")
+        data = json.loads(resp_one.data.decode())
+        assert resp_one.status_code == 200
+        assert len(data["data"]["users"]) == 1
+        resp_two = client.delete(f"/users/{user.id}")
+        data = json.loads(resp_two.data.decode())
+        assert resp_two.status_code == 200
+        assert "remove-me@email.io was removed!" in data["message"]
+        assert "success" in data["status"]
+        resp_three = client.get("/users")
+        data = json.loads(resp_three.data.decode())
+        assert resp_three.status_code == 200
+        assert len(data["data"]["users"]) == 0
+
+    def test_remove_user_incorrect_id(self, client, test_db):
+        resp = client.delete("/users/999")
+        data = json.loads(resp.data.decode())
+        assert resp.status_code == 404
+        assert "User does not exist" in data["message"]
+        assert "fail" in data["status"]
+
+    def test_update_user(self, client, test_db):
+        user = add_user("user-to-be-updated", "update-me@email.io")
+        resp_one = client.put(
+            f"/users/{user.id}",
+            data=json.dumps({"username": "me", "email": "me@email.io"}),
+            content_type="application/json",
+        )
+        data = json.loads(resp_one.data.decode())
+        assert resp_one.status_code == 200
+        assert f"{user.id} was updated!" in data["message"]
+        assert "success" in data["status"]
+        resp_two = client.get(f"/users/{user.id}")
+        data = json.loads(resp_two.data.decode())
+        assert resp_two.status_code == 200
+        assert "me" in data["data"]["username"]
+        assert "me@email.io" in data["data"]["email"]
+        assert "success" in data["status"]
+
+    def test_update_user_invalid_json(self, client, test_db):
+        resp = client.put(
+            "/users/1", data=json.dumps({}), content_type="application/json"
+        )
+        data = json.loads(resp.data.decode())
+        assert resp.status_code == 400
+        assert "Invalid payload." in data["message"]
+        assert "fail" in data["status"]
+
+    def test_update_user_invalid_json_keys(self, client, test_db):
+        resp = client.put(
+            "/users/1",
+            data=json.dumps({"email": "me@email.io"}),
+            content_type="application/json",
+        )
+        data = json.loads(resp.data.decode())
+        assert resp.status_code == 400
+        assert "Invalid payload." in data["message"]
+        assert "fail" in data["status"]
+
+    def test_update_user_does_not_exist(self, client, test_db):
+        resp = client.put(
+            "/users/999",
+            data=json.dumps({"username": "me", "email": "me@email.io"}),
+            content_type="application/json",
+        )
+        data = json.loads(resp.data.decode())
+        assert resp.status_code == 404
+        assert "User does not exist" in data["message"]
+        assert "fail" in data["status"]
